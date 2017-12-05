9dca988f9540d844162fef9da1ad890779523afe
39253 <392539910@qq.com>
2017-12-05 10:43:02 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+let  rd = require('rd');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-router.get('/a', function(req, res) {
-  res.render('a', { title: 'a' });
+//同步遍历目录下的所有文件
+rd.eachSync('views', function (f, s) {     //f文件名
+  if(f!=`f:\\web1\\views`){
+    let rF = f.split(`f:\\web1\\views\\`)[1];
+    let rF2 =   rF.split('.')[0];
+    router.get('/'+rF, function(req, res) {
+      res.render(rF, { title: rF });
+    });
+    router.get('/'+rF2, function(req, res) {
+      res.render(rF, { title: rF });
+    });
+  }
 });
 module.exports = router;
