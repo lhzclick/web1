b2f125afe7a2acd6eb7f38a7db1ebca5e9d0b8ac
39253 <392539910@qq.com>
2017-12-05 17:57:49 +0800

    1

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+let mysql =require('mysql');
+let dataJ ;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,13 +25,54 @@ app.use(express.static(path.join(__dirname, 'public')));  //配置公共静态文件
 app.use('/', routes);
 app.use('/users', users);
 app.use('/v', express.static('views'));   //配置项目静态文件
-/// catch 404 and forwarding to error handler
+//mysql创建连接
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    port:'3306',
+    database:'node'
+})
+//执行连接
+connection.connect(function(err){
+    if(err){
+        console.log('[query]-:'+err);
+    }else{
+        console.log('conneting')
+    }
+})
+//执行查询
+let userGetSql = 'SELECT * FROM info';
+    connection.query(userGetSql,function(err,result){
+        if(err){
+            console.log('查询错误')
+        }else{
+            dataJ = result;
+        }
+    });
+
+//connection.end();
+//查询
+app.get('/info', function (req, res) {
+    res.send(dataJ);
+});
+//新增
+app.get('/add',function (req,res){
+    let querys = req.query;
+})
+
+
+
+
+
+
+
+// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
 /// error handlers
 
 // development error handler
