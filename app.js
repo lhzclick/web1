8794c5c69835be64b3a67a710219496d573c6924
39253 <392539910@qq.com>
2017-12-05 11:55:20 +0800

    1

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/v', express.static('views'));
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
