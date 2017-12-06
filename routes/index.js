75d4fcc676b0fb270125e4dd9611d63d65884a75
39253 <392539910@qq.com>
2017-12-06 09:49:40 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 //同步遍历目录下的所有文件    配置view文件下所有路由
-rd.eachSync('views', function (f, s) {     //f文件名
+rd.eachSync('../views', function (f, s) {     //f文件名
   if(f.indexOf('.ejs')!=-1){
     let rF = f.split(`f:\\web1\\views\\`)[1].replace(/\\/g, "/");      //反斜杠转正斜杠
     let rF2 =   rF.split('.')[0];
