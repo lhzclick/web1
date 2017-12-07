277afe02941f5d23ca8e47663a0c645696b015fa
39253 <392539910@qq.com>
2017-12-07 14:45:06 +0800

    express+mysql增删改查

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-//同步遍历目录下的所有文件    配置view文件下所有路由
-rd.eachSync('./views', function (f, s) {     //f文件名
+//鍚屾閬嶅巻鏂囦欢
+rd.eachSync('./views', function (f, s) {
   if(f.indexOf('.ejs')!=-1){
-    let rF = f.split(`f:\\web1\\views\\`)[1].replace(/\\/g, "/");      //反斜杠转正斜杠
+    let rF = f.split(`f:\\web1\\views\\`)[1].replace(/\\/g, "/");      //姝ｅ弽鏂滄潬杞崲s
     let rF2 =   rF.split('.')[0];
     router.get('/'+rF, function(req, res) {
       res.render(rF, { title: rF });s
